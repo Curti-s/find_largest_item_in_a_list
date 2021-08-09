59c849fe53ffead9bb9bd3b0b39a0e165b1d548a
k1r1m1 <kirimi@paygoenergy.co>
2021-08-09 23:43:46 +0300

    defined fn largest_element(...) that does not implement the Copy trait.

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,38 @@ fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     largest
 }
 
+fn largest_element<T: PartialOrd> (list: &[T]) -> &T {
+    let mut index = 0;
+    let mut largest = &list[index];
+    let it = list.len() - 1;
+
+    while index <= it {
+        if index + 1 < it {
+            largest = if list[index] > list[index + 1] {
+                &list[index]
+            } else {
+                &list[index + 1]
+            };
+        }
+        index += 1;
+    }
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 20, 100, 65];
 
     let result = largest(&number_list);
-    println!("The largest nuber is {}", result);
+    println!("The largest number is {}", result);
 
     let char_list = vec!['y','k', 'j', 'l'];
     let result = largest(&char_list);
     println!("The largest char is {}", result);
+
+    let result = largest_element(&number_list);
+    println!("The largest element is {}", result);
+    
+    let char_list = vec!['y','k', 'j', 'l'];
+    let result = largest_element(&char_list);
+    println!("The largest char element is {}", result);
 }
